e83ee2667276cdcf2a42e0ee91ac5707afeca815
Luis Bezerra <52874192+joseluisbezerra@users.noreply.github.com>
2020-12-14 11:25:19 -0300

    Arquivos estáticos e layouts

---
+++ resources/views/contato.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title', 'Contato')
+
+@section('content')
+<h1>Essa é a página de contato</h1>
+
+<a href="/">Voltar para home</a>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title')</title>
+
+    <!-- Fonte do Google -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+    <!-- CSS Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!-- CSS customizados -->
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+    
+    @yield('content')
+    
+    <footer>
+        <p>HDC Events &copy; 2020</p>
+    </footer>
+
+
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
+    <!-- Javascript -->
+    <script src="/js/scripts.js"></script>
+</body>
+</html>
